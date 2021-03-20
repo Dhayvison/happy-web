f55fa20af2e69b028429eaef290155849e04e714
Dhayvison Braga <dhayvison.braga@hostweb.com.br>
2021-03-19 21:45:21 -0300

    chore: organize routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,9 +11,9 @@ export default function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={Landing} />
-        <Route path='/orphanages' exact component={OrphanagesMap} />
         <Route path='/orphanages/create' exact component={CreateOrphanage} />
         <Route path='/orphanages/:id' component={Orphanages} />
+        <Route path='/orphanages' component={OrphanagesMap} />
       </Switch>
     </BrowserRouter>
   );
